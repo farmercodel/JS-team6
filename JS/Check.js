5d2000fb7c7409ef6e1ec2b99001ce839972f3d0
cordelia <137859418+farmercodel@users.noreply.github.com>
2024-11-28 00:57:30 +0900

    전화번호 확인 기능 추가

--- JS/Check.js
+++ JS/Check.js
@@ -2,8 +2,18 @@
 const nameDisplay = document.getElementById("nameList");
 const userName = localStorage.getItem("userName");
 
+
+const phoneDisplay = document.getElementById("savedPhoneNumber");
+const fullNumber = localStorage.getItem("savedPhoneNumber")
+
 if (userName) {
     nameDisplay.innerText = userName; // 로컬 스토리지에서 가져온 이름 출력
 } else {
     nameDisplay.innerText = "입력한 이름이 없습니다."; // 이름이 없을 경우 메시지
+}
+
+if (fullNumber) {
+    phoneDisplay.innerText = fullNumber; 
+} else {
+    phoneDisplay.innerText = "입력한 전화번호가 없습니다.";
 }
--- JS/Phone.js
+++ JS/Phone.js
@@ -1,20 +1,22 @@
 function padNumber(num, length) {
-return String(num).padStart(length, '0');
+    return String(num).padStart(length, '0');
 }
 
 function increment(displayId, max) {
-const display = document.getElementById(displayId);
-let current = parseInt(display.textContent);
-current = current >= max ? 0 : current + 1;
-const length = max.toString().length;
-display.textContent = padNumber(current, length);
+    const display = document.getElementById(displayId);
+    let current = parseInt(display.textContent);
+    current = current >= max ? 0 : current + 1;
+    const length = max.toString().length;
+    display.textContent = padNumber(current, length);
 }
 
 function saveNumber() {
-const phone1 = document.getElementById('display1').textContent;
-const phone2 = document.getElementById('display2').textContent;
-const phone3 = document.getElementById('display3').textContent;
+    const phone1 = document.getElementById('display1').textContent;
+    const phone2 = document.getElementById('display2').textContent;
+    const phone3 = document.getElementById('display3').textContent;
 
-const fullNumber = `${ phone1 } - ${ phone2 } - ${ phone3 }`;
-document.getElementById('savedPhoneNumber').innerText = fullNumber;
+    const fullNumber = `${ phone1 } - ${ phone2 } - ${ phone3 }`;
+    document.getElementById('savedPhoneNumber').innerText = fullNumber;
+    localStorage.setItem('savedPhoneNumber', fullNumber);
+    signUpButton.disabled = false;
 }
